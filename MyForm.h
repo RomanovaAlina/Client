595883da56843a1b81eec8adc1ad58bb67bdc2fb
RomanovaAlina <romanova.ailna@mail.ru>
2017-03-19 11:01:27 +0300

    Client v2

--- MyForm.h
+++ MyForm.h
@@ -4,6 +4,7 @@
 #include <iostream> //для вывода информации в консоль
 #include <WS2tcpip.h> //для получения информации о сервере
 #include <string>
+//#include <pthread.h>
 #include "MyForm1.h"
 #include <msclr\marshal_cppstd.h>
 using namespace System;
@@ -42,6 +43,7 @@ namespace Project {
 		Thread^th;
 	private: System::Windows::Forms::TextBox^  textBox1;
 	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::Label^  label2;
 	public: 
 	private: System::Windows::Forms::Button^  button2;
 	public:
@@ -56,16 +58,13 @@ namespace Project {
 		{
 			if (message[0]!=0)
 			{
-				//array <Byte>^buffer;
-				//buffer=Encoding::UTF8->GetBytes(message);
 				char* buffer=new char[1024];
 				std::strcpy(buffer,message.c_str());
-				//Client.Send(buffer);
-				send(socket(AF_INET, SOCK_STREAM, IPPROTO_TCP),buffer,strlen(buffer),NULL);
+				send(socket(AF_UNSPEC, SOCK_STREAM, IPPROTO_TCP),buffer,strlen(buffer),NULL);
 			}
 		}
 
-		  void RecvMessage() // функция принятия сообщений от клиента
+		  void* RecvMessage(void *args) // функция принятия сообщений от клиента
         {
 			//array<Byte>^ buffer;
 			char* buffer=new char [1024];
@@ -76,7 +75,7 @@ namespace Project {
             for (; ; )
             {
                    //Client.receive(buffer);//прием сообщения с помощью именно сокета, в виде байтовых сиволов
-				   recv(socket(AF_INET, SOCK_STREAM,IPPROTO_TCP),buffer,1024,NULL);
+				   recv(socket(AF_UNSPEC, SOCK_STREAM,IPPROTO_TCP),buffer,1024,NULL);
 				   //String^ message;//перевод байтов в строку
 				   //message=Encoding::UTF8->GetString(buffer);
 				   string message(buffer);
@@ -110,7 +109,7 @@ namespace Project {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^  button1;
+
 	protected: 
 
 	private:
@@ -119,7 +118,7 @@ namespace Project {
 #pragma region Windows Form Designer generated code
 		void InitializeComponent(void)
 		{
-			this->button1 = (gcnew System::Windows::Forms::Button());
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
 			this->richTextBox2 = (gcnew System::Windows::Forms::RichTextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
@@ -129,18 +128,9 @@ namespace Project {
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
-			// 
-			// button1
-			// 
-			this->button1->Location = System::Drawing::Point(377, 12);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(122, 59);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"Exit";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// richTextBox1
 			// 
@@ -161,18 +151,26 @@ namespace Project {
 			// label1
 			// 
 			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
+			this->label1->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
+			this->label1->Image = (cli::safe_cast<System::Drawing::Image^  >(resources->GetObject(L"label1.Image")));
 			this->label1->Location = System::Drawing::Point(25, 302);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(112, 13);
+			this->label1->Size = System::Drawing::Size(146, 19);
 			this->label1->TabIndex = 3;
 			this->label1->Text = L"Введите сообщение:";
 			// 
 			// menuStrip1
 			// 
+			this->menuStrip1->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->menuStrip1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^  >(resources->GetObject(L"menuStrip1.BackgroundImage")));
+			this->menuStrip1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
 			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) {this->toolStripMenuItem1});
 			this->menuStrip1->Location = System::Drawing::Point(0, 0);
 			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->Size = System::Drawing::Size(526, 24);
+			this->menuStrip1->Size = System::Drawing::Size(526, 27);
 			this->menuStrip1->TabIndex = 4;
 			this->menuStrip1->Text = L"menuStrip1";
 			// 
@@ -180,21 +178,25 @@ namespace Project {
 			// 
 			this->toolStripMenuItem1->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) {this->войтиToolStripMenuItem});
 			this->toolStripMenuItem1->Name = L"toolStripMenuItem1";
-			this->toolStripMenuItem1->Size = System::Drawing::Size(53, 20);
+			this->toolStripMenuItem1->Size = System::Drawing::Size(63, 23);
 			this->toolStripMenuItem1->Text = L"Меню";
 			// 
 			// войтиToolStripMenuItem
 			// 
 			this->войтиToolStripMenuItem->Name = L"войтиToolStripMenuItem";
-			this->войтиToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->войтиToolStripMenuItem->Size = System::Drawing::Size(121, 24);
 			this->войтиToolStripMenuItem->Text = L"Войти";
 			this->войтиToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::войтиToolStripMenuItem_Click);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(383, 95);
+			this->button2->BackgroundImage = (cli::safe_cast<System::Drawing::Image^  >(resources->GetObject(L"button2.BackgroundImage")));
+			this->button2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
+			this->button2->ForeColor = System::Drawing::SystemColors::ButtonFace;
+			this->button2->Location = System::Drawing::Point(377, 95);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(115, 56);
+			this->button2->Size = System::Drawing::Size(75, 26);
 			this->button2->TabIndex = 5;
 			this->button2->Text = L"Войти";
 			this->button2->UseVisualStyleBackColor = true;
@@ -202,33 +204,55 @@ namespace Project {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(383, 186);
+			this->textBox1->Location = System::Drawing::Point(375, 69);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(131, 20);
 			this->textBox1->TabIndex = 6;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(388, 316);
+			this->button3->BackgroundImage = (cli::safe_cast<System::Drawing::Image^  >(resources->GetObject(L"button3.BackgroundImage")));
+			this->button3->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
+			this->button3->ForeColor = System::Drawing::SystemColors::ButtonFace;
+			this->button3->Location = System::Drawing::Point(355, 325);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(109, 34);
+			this->button3->Size = System::Drawing::Size(109, 26);
 			this->button3->TabIndex = 7;
 			this->button3->Text = L"Отправить";
 			this->button3->UseVisualStyleBackColor = true;
 			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
 			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::SystemColors::AppWorkspace;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
+			this->label2->ForeColor = System::Drawing::SystemColors::ButtonFace;
+			this->label2->Image = (cli::safe_cast<System::Drawing::Image^  >(resources->GetObject(L"label2.Image")));
+			this->label2->ImageAlign = System::Drawing::ContentAlignment::MiddleRight;
+			this->label2->Location = System::Drawing::Point(374, 44);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(132, 19);
+			this->label2->TabIndex = 8;
+			this->label2->Text = L"Введите ваше имя";
+			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^  >(resources->GetObject(L"$this.BackgroundImage")));
 			this->ClientSize = System::Drawing::Size(526, 375);
+			this->Controls->Add(this->label2);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->richTextBox2);
 			this->Controls->Add(this->richTextBox1);
-			this->Controls->Add(this->button1);
 			this->Controls->Add(this->menuStrip1);
 			this->MainMenuStrip = this->menuStrip1;
 			this->Name = L"MyForm";
@@ -248,33 +272,45 @@ namespace Project {
 	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) //кнопка "войти"
 			 {
 				if (textBox1->Text != " " && textBox1->Text != "")
-            {
-				SOCKET ConnectSocket = INVALID_SOCKET; 
-				int iResult; 
-				struct addrinfo *result = NULL, *ptr = NULL, hints;
-				SOCKADDR_IN anAddr;
-				anAddr.sin_family = AF_UNSPEC;
-				anAddr.sin_port = htons(30);
-				//anAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+				{
+					SOCKET ConnectSocket = INVALID_SOCKET; 
+					int iResult; 
+
+					WSADATA wsaData; 
+					struct addrinfo *result = NULL, *ptr = NULL, hints;
  
-				ZeroMemory(&hints, sizeof(hints)); 
-				hints.ai_family = AF_UNSPEC; 
-				hints.ai_socktype = SOCK_STREAM; 
-				hints.ai_protocol = IPPROTO_TCP; 
-				hints.ai_addr="127.0.0.1";
+					iResult = WSAStartup(MAKEWORD(2, 2), &wsaData); 
 
-				iResult = getaddrinfo("127.0.0.1",DEFAULT_PORT, &hints, &result); 
-				ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol); 
-				iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen); 
+					ZeroMemory(&hints, sizeof(hints)); 
+					hints.ai_family = AF_UNSPEC; 
+					hints.ai_socktype = SOCK_STREAM; 
+					hints.ai_protocol = IPPROTO_TCP; 
 
-                richTextBox1->Enabled = false;
-               //if (ip!=NULL)
-               // {
-				//connect(socket(AF_UNSPEC, SOCK_STREAM,IPPROTO_TCP),(struct sockaddr *)&anAddr, sizeof(struct sockaddr));//подключение к серверу
-				//CreateThread(NULL,NULL,&RecvMessage,NULL,NULL,NULL);
+					iResult = getaddrinfo("127.0.0.1","7770", &hints, &result); 
 
-               // }
-            }
+					for (ptr = result; ptr != NULL; ptr = ptr->ai_next) 
+					{ 
+						ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol); 
+
+						iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen); //подключение к серверу
+						if (iResult == SOCKET_ERROR) 
+						{ 
+							closesocket(ConnectSocket); 
+							ConnectSocket = INVALID_SOCKET; 
+							continue; 
+						} 
+						break; 
+					} 
+					freeaddrinfo(result);
+
+					richTextBox1->Enabled = false;
+					richTextBox1->AppendText("Hello");
+					//new th([&]() { this.RecvMessage(); })
+					//CreateThread(NULL,NULL,&RecvMessage,NULL,NULL,NULL);
+					//pthread_t t;
+					//pthread_create(&t, NULL, RecvMessage, NULL);
+					//pthread_join(t, NULL);
+				}
 			 }
 private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) //кнопка "отправить"
 		 {
@@ -287,6 +323,10 @@ private: System::Void войтиToolStripMenuItem_Click(System::Object^  sender, Syst
 		 {
 			 MyForm1 ^f2=gcnew MyForm1();
 			 f2->ShowDialog();
+		 }
+private: System::Void label2_Click(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
 		 }
 };
 }
--- MyForm1.h
+++ MyForm1.h
@@ -60,7 +60,7 @@ namespace Project {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(42, 80);
+			this->label1->Location = System::Drawing::Point(60, 9);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(108, 13);
 			this->label1->TabIndex = 0;
@@ -69,19 +69,20 @@ namespace Project {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(12, 96);
+			this->textBox1->Location = System::Drawing::Point(12, 36);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(185, 20);
 			this->textBox1->TabIndex = 1;
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(221, 96);
+			this->button1->Location = System::Drawing::Point(221, 35);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(51, 20);
 			this->button1->TabIndex = 2;
-			this->button1->Text = L"button1";
+			this->button1->Text = L"Войти";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm1::button1_Click);
 			// 
 			// MyForm1
 			// 
@@ -93,12 +94,17 @@ namespace Project {
 			this->Controls->Add(this->label1);
 			this->Name = L"MyForm1";
 			this->Text = L"MyForm1";
+			this->Load += gcnew System::EventHandler(this, &MyForm1::MyForm1_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+			 }
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+			 }
+	private: System::Void MyForm1_Load(System::Object^  sender, System::EventArgs^  e) {
 			 }
 	};
 }
